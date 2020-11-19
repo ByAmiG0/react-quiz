0d3b79687566959f475c0621f9458ea47ebc9eaf
Ahmet Çayırezmez <ahmet.cayirezmez@akinon.com>
2020-11-19 23:30:17 +0300

    endpoint rev

--- src/API.ts
+++ src/API.ts
@@ -18,7 +18,7 @@ export enum Difficulty {
 }
 
 export const fetchQuizQuestions = async (amount:number, difficulty: Difficulty) => {
-  const endPoint = `http://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endPoint)).json();
   return data.results.map((question: Question) => (
     {
